522dade28425b9cf40d8a4bcdd1b8e64f128fbf2
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-12-28 08:36:38 +0100

    write ftoc (fahrenheit to celcius) function

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
-#write your code here
+# frozen_string_literal: true
+
+# write your code here
+
+def ftoc(degree_fahrenheit)
+  degree_fahrenheit_float = degree_fahrenheit.to_f
+  (degree_fahrenheit_float - 32) * 5.0 / 9.0
+end
