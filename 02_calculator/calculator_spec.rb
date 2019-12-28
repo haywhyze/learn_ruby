c620d39be4a7d50b2533ee3df7ebd2cad000b3f9
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-12-28 09:11:32 +0100

    add tests for multiply

--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -79,10 +79,13 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+    expect(multiply([4,5])).to eq(20)
+  end  
 
-  it "multiplies several numbers"
-  
+  it "multiplies several numbers" do
+    expect(multiply([2,3,4,2])).to  eq(48)
+  end
 end
 
 describe "#power" do
