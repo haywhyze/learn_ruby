89776bacdb53e510758ce1b01313e63b8d7cdd47
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-12-28 08:37:28 +0100

    write ctof (celcius to fahrenheit) function

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -6,3 +6,8 @@ def ftoc(degree_fahrenheit)
   degree_fahrenheit_float = degree_fahrenheit.to_f
   (degree_fahrenheit_float - 32) * 5.0 / 9.0
 end
+
+def ctof(degree_celcius)
+  degree_celcius_float = degree_celcius.to_f
+  degree_celcius_float * 9.0 / 5.0 + 32
+end
