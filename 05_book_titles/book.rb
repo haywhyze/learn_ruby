ab07bddc034102d3fc6bc0d641d5a99ee76d1254
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-12-29 13:59:54 +0100

    implement title attribute writer

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
+# frozen_string_literal: true
+
 class Book
-# write your code here
+  # write your code here
+
+  attr_writer :title
+
+  def title
+    @title.split.each_with_index.map do |x, i|
+      small_words = %w[the over and in a to an of on]
+      if !small_words.include?(x) || i.zero?
+        x.capitalize
+      else
+        x
+      end
+    end .join(' ')
+  end
 end
